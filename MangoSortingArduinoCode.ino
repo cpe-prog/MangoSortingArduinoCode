3e14eb3ad689c3152cc1f83847f3e0b58093a6ce
Grian Gajila <griangajila2@gmail.com>
2024-05-27 14:45:19 +0800

    wip

--- MangoSortingArduinoCode.ino
+++ MangoSortingArduinoCode.ino
@@ -7,13 +7,21 @@
 #include "addons/TokenHelper.h"
 #include "addons/RTDBHelper.h"
 
-HX711_ADC LoadCell(7, 8); 
+#define DOUT  D6
+#define CLK  D7
+ 
+HX711_ADC scale(DOUT, CLK); 
 
 Servo s1;
 Servo s2;
 Servo s3;
 Servo s4;
 Servo s5;
+
+// int previousMediumRawValue = -1;
+// int previousLargeRawValue = -1;
+// int previousMediumRipeValue = -1;
+// int previousLargeRipeValue = -1;
 
 String lastCategory = "";
 unsigned long categoryStartTime = 0;
@@ -32,6 +40,10 @@ FirebaseConfig config;
 unsigned long sendDataPrevMillis = 0;
 bool signupOK = false;
 bool mangoStatus = false;
+bool servo2Active = false;
+bool servo3Active = false;
+bool servo4Active = false;
+bool servo5Active = false;
 
 void setup() {
   Serial.begin(9600);
@@ -40,9 +52,9 @@ void setup() {
   s3.attach(D2);
   s4.attach(D3);
   s5.attach(D4);
-  LoadCell.begin(); // start connection to HX711
-  LoadCell.start(2000); // load cells gets 2000ms of time to stabilize
-  LoadCell.setCalFactor(1000.0); // calibration factor for load cell => dependent on your individual setup
+  scale.begin(); 
+  scale.start(2000); 
+  scale.setCalFactor(1000.0);
   Serial.println("Scale is ready");
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to Wi-Fi");
@@ -73,52 +85,144 @@ void setup() {
 
 }
 
+void resetServos() {
+  s2.write(0);
+  s3.write(0);
+  s4.write(0);
+  s5.write(0);
+  servo2Active = false;
+  servo3Active = false;
+  servo4Active = false;
+  servo5Active = false;
+}
+
+
 
 void loop() {
-  LoadCell.update();
-  float weight = LoadCell.getData(); 
-  String currentCategory = "";
-  Serial.println(weight);
-  if (weight >= 180 && weight <= 249) {
-    currentCategory = "small";
-  } else if (weight >= 250 && weight <= 349) {
-    currentCategory = "medium";
-  } else if (weight >= 350 && weight <= 400) {
-    currentCategory = "large";
-  }
-  if (currentCategory != lastCategory) {
-    lastCategory = currentCategory;
-    categoryStartTime = millis(); 
-  } else if (currentCategory != "" && millis() - categoryStartTime >= categoryStableTime) {
-    Serial.println(currentCategory);
-    LoadCell.tare();
-  }
-
-  if (Firebase.ready() && signupOK && (millis() -  sendDataPrevMillis > 1000 || sendDataPrevMillis == 0 )) {
+  if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 5 || sendDataPrevMillis == 0)) {
     sendDataPrevMillis = millis();
-    if (Firebase.RTDB.getBool(&fbdo, "mango/1/maturity")){
-      if (fbdo.dataType() == "boolean"){
-      mangoStatus = fbdo.boolData();
-      Serial.println("Seccess: " + fbdo.dataPath() + ": " + mangoStatus + "(" + fbdo.dataType() + ")");
-        if (mangoStatus == true){
-          Serial.print("Servo Running to 180 ");
-          s1.write(180);
-          if (currentCategory = "medium"){// adding condition which if current category is equal to small, medium or large which the servo for ripe will be working depend for medium and large
+    scale.update();
+    float weight = scale.getData(); 
+    String currentCategory = "";
+    Serial.println(weight);
+    if (weight >= 180 && weight <= 249) {
+      currentCategory = "small";
+    } else if (weight >= 250 && weight <= 349) {
+      currentCategory = "medium";
+    } else if (weight >= 350 && weight <= 400) {
+      currentCategory = "large";
+    }
+    if (currentCategory != lastCategory) {
+      lastCategory = currentCategory;
+      categoryStartTime = millis(); 
+    } else if (currentCategory != "" && millis() - categoryStartTime >= categoryStableTime) {
+      Serial.println(currentCategory);
+      scale.tare();
+    }
+    
+    if (Firebase.RTDB.getBool(&fbdo, "mango/1/maturity")) {
+      if (fbdo.dataType() == "boolean") {
+        mangoStatus = fbdo.boolData();
+        Serial.println("Success: " + fbdo.dataPath() + ": " + mangoStatus + " (" + fbdo.dataType() + ")");
+        if (mangoStatus == true) {
+          if (currentCategory == "medium" && !servo2Active) {
             s2.write(180);
-          } 
-        }
-        else{
-          Serial.print("Servo Running to 0 "); 
-          s1.write(0);
-          // adding condition which if current category is equal to small, medium or large which the servo for raw will be working depend for medium and large
+            Serial.println("Medium Ripe is Rotating");
+            servo2Active = true;
+          } else if (currentCategory == "large" && !servo3Active) {
+            s3.write(180);
+            Serial.println("Large Ripe is Rotating");
+            servo3Active = true;
+          }
+        } else {
+          if (currentCategory == "medium" && !servo4Active) {
+            s4.write(180);
+            Serial.println("Medium Raw is Rotating");
+            servo4Active = true;
+          } else if (currentCategory == "large" && !servo5Active) {
+            s5.write(180);
+            Serial.println("Large Raw is Rotating");
+            servo5Active = true;
+          }
         }
       }
-    }
-    else {
+    } else {
       Serial.println("FAILED: " + fbdo.errorReason());
     }
   }
 
-
-
+  if (s1.read() == 0 && s2.read() == 0 && s3.read() == 0 && s4.read() == 0 && s5.read() == 0) {
+    resetServos();
+  }
 }
+
+// void mediumRawChecker() {
+//     if (Firebase.RTDB.getInt(&fbdo, "raw/1/medium")) {
+//         if (fbdo.dataType() == "int") {
+//             int currentMediumRawValue = fbdo.intData();
+//             if (currentMediumRawValue != previousMediumRawValue) {
+//                 Serial.print("Small raw value changed to: ");
+//                 Serial.println(currentMediumRawValue);
+//                 s4.write(0);
+//                 previousMediumRawValue = currentMediumRawValue;
+//             }
+//         }
+//     } else {
+//         Serial.println("Failed to get count from Firebase");
+//         Serial.println("REASON: " + fbdo.errorReason());
+//     }
+// }
+
+// void largeRawChecker() {
+//     if (Firebase.RTDB.getInt(&fbdo, "raw/1/large")) {
+//         if (fbdo.dataType() == "int") {
+//             int currentLargeRawValue = fbdo.intData();
+//             if (currentLargeRawValue != previousLargeRawValue) {
+//                 Serial.print("Small raw value changed to: ");
+//                 Serial.println(currentLargeRawValue);
+//                 s5.write(0);
+//                 previousLargeRawValue = currentLargeRawValue;
+//             }
+//         }
+//     } else {
+//         Serial.println("Failed to get count from Firebase");
+//         Serial.println("REASON: " + fbdo.errorReason());
+//     }
+// }
+
+// void mediumRipeChecker() {
+//     if (Firebase.RTDB.getInt(&fbdo, "ripe/1/medium")) {
+//         if (fbdo.dataType() == "int") {
+//             int currentMediumRipeValue = fbdo.intData();
+//             if (currentMediumRipeValue != previousMediumRipeValue) {
+//                 Serial.print("Small raw value changed to: ");
+//                 Serial.println(currentMediumRipeValue);
+//                 s2.write(0);
+//                 previousMediumRipeValue = currentMediumRipeValue;
+//             }
+//         }
+//     } else {
+//         Serial.println("Failed to get count from Firebase");
+//         Serial.println("REASON: " + fbdo.errorReason());
+//     }
+// }
+
+// void largeRipeChecker() {
+//     if (Firebase.RTDB.getInt(&fbdo, "ripe/1/large")) {
+//         if (fbdo.dataType() == "int") {
+//             int currentLargeRipeValue = fbdo.intData();
+//             if (currentLargeRipeValue != previousLargeRipeValue) {
+//                 Serial.print("Small raw value changed to: ");
+//                 Serial.println(currentLargeRipeValue);
+//                 s3.write(0);
+//                 previousLargeRipeValue = currentLargeRipeValue;
+//             }
+//         }
+//     } else {
+//         Serial.println("Failed to get count from Firebase");
+//         Serial.println("REASON: " + fbdo.errorReason());
+//     }
+// }
+
+
+
